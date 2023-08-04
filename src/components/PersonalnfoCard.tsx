ab13b427f8cfc1b4904d725a3b2a47ac7324d3ce
AlexCiobanu47 <alexciobanu72@yahoo.com>
2023-08-04 14:37:16 +0300

    added personal information validation

--- src/components/PersonalnfoCard.tsx
+++ src/components/PersonalnfoCard.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent } from "react";
+import React, { ChangeEvent, useState } from "react";
 
 interface PersonalInfoCardProps {
   setName: (name: string) => void;
@@ -12,18 +12,40 @@ const PersonalnfoCard: React.FC<PersonalInfoCardProps> = ({
   setPhone,
   nextState,
 }) => {
+  const [nameError, setNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [phoneNumberError, setPhoneNumberError] = useState("");
   const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const pattern: RegExp = /\w\s\w/;
+    if (!pattern.test(event.target.value)) {
+      setNameError("Enter a valid name");
+    } else {
+      setNameError("");
+    }
     setName(event.target.value);
   };
   const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const pattern: RegExp = /\w@\w/;
+    if (!pattern.test(event.target.value)) {
+      setEmailError("Enter a valid email");
+    } else {
+      setEmailError("");
+    }
     setEmail(event.target.value);
   };
   const handlePhoneChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const pattern: RegExp = /\d{10}/;
+    if (!pattern.test(event.target.value)) {
+      setPhoneNumberError("Enter a valid phone number");
+    } else {
+      setPhoneNumberError("");
+    }
     setPhone(event.target.value);
   };
   const handleNextStep = () => {
     nextState();
   };
+
   return (
     <div className="z-50 bg-white p-5 m-5 rounded-lg  flex flex-col">
       <div>
@@ -33,35 +55,49 @@ const PersonalnfoCard: React.FC<PersonalInfoCardProps> = ({
         </p>
         <form>
           <div className="flex flex-col">
-            <label className="text-blue">Name</label>
+            <div className="flex justify-between">
+              <label className="text-blue">Name</label>
+              {nameError && <span className="text-red-500">{nameError}</span>}
+            </div>
             <input
               type="text"
               placeholder="e.g. Stephen King"
               onChange={handleNameChange}
-              className="border border-LightGray px-2 py-1 font-semibold"
+              className={`border-2 rounded-md px-2 py-1 font-semibold 
+              ${nameError ? "border-red-500" : "border-LightGray"}`}
             />
           </div>
           <div className="flex flex-col">
-            <label>Email address</label>
+            <div className="flex justify-between">
+              <label>Email address</label>
+              {emailError && <span className="text-red-500">{emailError}</span>}
+            </div>
             <input
               type="text"
               placeholder="e.g. stephenking@lorem.com"
               onChange={handleEmailChange}
-              className="border border-LightGray px-2 py-1 font-semibold"
+              className={`border-2 rounded-md px-2 py-1 font-semibold 
+              ${emailError ? "border-red-500" : "border-LightGray"}`}
             />
           </div>
           <div className="flex flex-col">
-            <label>Phone number</label>
+            <div className="flex justify-between">
+              <label>Phone number</label>
+              {phoneNumberError && (
+                <span className="text-red-500">{phoneNumberError}</span>
+              )}
+            </div>
             <input
               type="text"
               placeholder="e.g. +1 234 567 890"
               onChange={handlePhoneChange}
-              className="border border-LightGray px-2 py-1 font-semibold"
+              className={`border-2 rounded-md px-2 py-1 font-semibold 
+              ${phoneNumberError ? "border-red-500" : "border-LightGray"}`}
             />
           </div>
         </form>
       </div>
-      <div className="flex items-center justify-end bottom-0 right-0">
+      <div className="flex items-center justify-end bottom-0 right-0 my-5">
         <button
           onClick={handleNextStep}
           className="text-white py-1 px-4 bg-MarineBlue rounded-md"
